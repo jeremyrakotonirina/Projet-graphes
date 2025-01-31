8cc1c5ecd466b3692d17f835f47f1180fefd3bcc
Eduardo Cobos Fernandez <eduardo.cobos-fernandez@etu-upsaclay.fr>
2025-01-31 11:29:28 +0000

    Worksheet après td1

--- worksheet.py
+++ worksheet.py
@@ -1,7 +1,12 @@
 from modules.open_digraph import *
 import inspect
 
+"""  Exercice 9 du TP1 
 
+print(f"\n Methodes de la classe node: \n {dir(node)}\n")
+print(f"\n Methodes de la classe node: \n {dir(open_digraph)}\n")
 
-
-
+print(f"\n Documentation de node.copy : \n {inspect.getdoc(node.copy)}\n")
+print(f"Lignes de code de node.copy : \n {inspect.getsourcelines(node.copy)}\n")
+print(f"Fichier ou se trouve node.copy : \n {inspect.getfile(node.copy)}\n")
+"""
