ecc690584cb136e942bdaaea12916c2d1c37c5f2
Eduardo Cobos Fernandez <eduardo.cobos-fernandez@etu-upsaclay.fr>
2025-03-14 23:26:28 +0000

    Replace worksheet.py par worksheet.py où je teste les fonctions dot

--- worksheet.py
+++ worksheet.py
@@ -11,4 +11,16 @@ print(f"Lignes de code de node.copy : \n {inspect.getsourcelines(node.copy)}\n")
 print(f"Fichier ou se trouve node.copy : \n {inspect.getfile(node.copy)}\n")
 """
 
+#test effectués pour voir si les dot fonctionnaient
+""" 
+G=open_digraph([],[],[])
+G = G.random(4, 3, form='loop-free')#graphe d'origine
+print(G)
+G.save_as_dot_file("test.dot", verbose=False)
+
+P=open_digraph([],[],[])
+P = P.from_dot_file("test.dot")
+print(P)
+G.display()
+"""
 
