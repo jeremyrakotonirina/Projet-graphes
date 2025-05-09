e20fb93407960cc25ff5327bf09117d7ae524be6
amine berrahma <amine.berrahma@etu-upsaclay.fr>
2025-05-09 20:43:06 +0000

    Remplacer GettersSetters.py

--- modules/open_digraph_mixin/GettersSetters.py
+++ modules/open_digraph_mixin/GettersSetters.py
@@ -1,4 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) #permet d'importer node.py
+
+from node import *
+
+
 class MethodesGettersSetters:
+    #---------------------------getter--------------------------
     def get_input_ids(self):
         """renvoie les id des inputs nodes"""
         return self.inputs
@@ -13,36 +21,32 @@ class MethodesGettersSetters:
    
     def get_nodes(self):
         """renvoie une liste de tous les noeuds"""
-        return [self.nodes[cle] for cle in self.nodes]
+        return list(self.nodes.values())
    
-    def get_node_ids(self, n):
-        """renvoie l'ID du noeud n"""
-        return [ID for ID,noeud in self.nodes.items() if n==noeud][0] #premier élément
-   
-    def get_node_by_id(self, ID):
+    def get_node_by_id(self, id):
         """renvoie le noeud d'id ID"""
-        return self.nodes[ID]
+        return self.nodes[id]
    
-    def get_nodes_by_ids(self, IDS):
+    def get_nodes_by_ids(self, ids):
         """renvoie une liste de noeuds à partir d'une liste d'ids"""
-        return [self.nodes[cle] for cle in self.nodes if cle in IDS]
+        return [self.nodes[id] for id in ids]
    
-    #-------------Setters---------------------------  
-    def set_inputs(self, new_inputs):
+    def get_node_ids(self):
+        """renvoie l'ID du noeud n"""
+        return list(self.nodes.keys())
+   #-----------------------------setter-----------------------------------
+    def set_input_ids(self, inputs):
         """Remplace la liste des entrées"""
-        self.inputs = new_inputs
-
-    def set_outputs(self, new_outputs):
+        self.inputs = inputs
+   
+    def set_output_ids(self, outputs):
         """Remplace la liste des sorties"""
-        self.outputs = new_outputs
+        self.outputs = outputs
    
-    def add_input_id(self, ID):
+    def add_input_id(self, id):
         """Ajoute un nouvel id dans la liste des entrées"""
-        if ID not in self.inputs:
-            self.inputs.append(ID)
+        self.inputs.append(id)
    
-    def add_output_id(self, ID):
+    def add_output_id(self, id):
         """Ajoute un nouvel id dans la liste des sorties"""
-        if ID not in self.outputs:
-            self.outputs.append(ID)
-   
+        self.outputs.append(id)
